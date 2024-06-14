b445d4e6106f36cd5a9e8b77abe10de9af111137
ne228 <potter27.05@mail.ru>
2024-06-14 11:56:44 +0300

    add Bulrog

---
+++ src/main/java/com/example/ais_ecc/munchkin/models/doorCards/enemyCards/Bulrog.java
@@ -0,0 +1,40 @@
+package com.example.ais_ecc.munchkin.models.doorCards.enemyCards;
+
+import com.example.ais_ecc.munchkin.models.Fight;
+import com.example.ais_ecc.munchkin.models.Player;
+import com.example.ais_ecc.munchkin.models.doorCards.EnemyCard;
+import com.example.ais_ecc.munchkin.service.MunchkinContext;
+import com.example.ais_ecc.munchkin.service.action.ActionDie;
+
+public class Bulrog extends EnemyCard {
+
+
+    public Bulrog(MunchkinContext munchkinContext) {
+        super(munchkinContext);
+        title = "Бульрог";
+        text = "Не преследует никого, чей Уровень 4 " +
+                "или ниже.";
+        obscenityText = "Непотребство: Застегал тебя до " +
+                "смерти.";
+        rewardLevel = 2;
+        rewardTreasure = 5;
+        level = 18;
+        this.iconPath = "/images/bulrog.png";
+    }
+
+    @Override
+    public void obscenity(Fight fight, Player player) throws Exception {
+        var act = new ActionDie(player);
+        if (act.canAmI(getMunchkinContext()))
+            getMunchkinContext().getActionHandler().doAction(act);
+    }
+
+    @Override
+    public boolean canChaise(Player player) {
+        if (player.getLvl() <= 4)
+            return false;
+
+        return true;
+    }
+
+}
--- src/main/java/com/example/ais_ecc/munchkin/service/CardInit.java
+++ src/main/java/com/example/ais_ecc/munchkin/service/CardInit.java
@@ -114,6 +114,7 @@ public class CardInit {
             i_player.getCards().add(new InsuranceAgent(context));
             i_player.getCards().add(new InsuranceAgent(context));
             i_player.getCards().add(new Bigfoot(context));
+            i_player.getCards().add(new Bulrog(context));
 
             i_player.getCards().add(new CalmadzillaEnemyCard(context));
             i_player.getCards().add(new Leprechaun(context));
